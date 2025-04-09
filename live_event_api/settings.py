95990458897e9c55db11402c6ba94eb88d379fd8
Lykkss <quaglierilisa3@gmail.com>
2025-04-09 22:32:45 +0200

    deploy

--- live_event_api/settings.py
+++ live_event_api/settings.py
@@ -58,11 +58,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://react-frontend-mspr2.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'live_event_api.urls'
