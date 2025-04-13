a496fef03148f410538ba8f4da9d0d6e17a0a153
Lykkss <quaglierilisa3@gmail.com>
2025-04-13 08:51:19 +0000

    clear old api

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-# api/admin.py
-from django.contrib import admin
-from .models import Organisateur, Lieu, Concert, Categorie
-
-admin.site.register(Organisateur)
-admin.site.register(Lieu)
-admin.site.register(Concert)
-admin.site.register(Categorie)
--- live_event_api/urls.py
+++
@@ -1,8 +0,0 @@
-# live_event_api/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-]
