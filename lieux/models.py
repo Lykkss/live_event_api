0393c64bd06065587048b30dc44ead294c54b580
Lykkss <quaglierilisa3@gmail.com>
2026-06-07 09:43:39 +0200

    fix: add toilettes/buvettes to Lieu model, fix gitignore

--- lieux/models.py
+++ lieux/models.py
@@ -4,6 +4,8 @@ class Lieu(models.Model):
     name = models.CharField(max_length=200)
     address = models.TextField()
     description = models.TextField()
+    toilettes = models.BooleanField(default=False)
+    buvettes = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
